87a1c6ea854449b2d0c2ffa1d8d717a32f2722ae
4 eye coder <83081348+4eyecoder-1997@users.noreply.github.com>
2023-03-09 12:56:47 +0545

    content update, footer added

---
+++ siyaan/faq.js
@@ -0,0 +1,29 @@
+const carpetFAQ = [
+    {
+        question: "What are the best carpet cleaning services in Australia?",
+        answer: "Siyaan Cleaning offers the best carpet cleaning services in Australia. Our team of experienced and certified cleaners are equipped with the latest technology and techniques to provide you with the best carpet cleaning services in the industry.",
+    },
+    {
+        question: "What is the difference between carpet dry cleaning and steam cleaning in Australia?",
+        answer: "Carpet dry cleaning is a process that uses minimal water and is ideal for busy households and offices. Steam cleaning, on the other hand, uses hot water and steam to clean your carpets thoroughly. Both methods are effective and the choice depends on your specific requirements."
+    },
+    {
+        question: "How much does commercial carpet cleaning cost in Australia?",
+        answer: "The cost of commercial carpet cleaning in Australia varies depending on the size of the space and the level of cleaning required. Contact Siyaan Cleaning today for a free quote.",
+    },
+    {
+        question: "How often should I get my carpets cleaned in Australia?",
+        answer: "The frequency of carpet cleaning in Australia depends on your specific requirements. For households with pets or small children, it is recommended to clean carpets at least twice a year. For commercial spaces, it may be necessary to clean carpets more frequently."
+    },
+    {
+        question: "How long does it take for carpets to dry after steam cleaning in Australia?",
+        answer: "The drying time for carpets after steam cleaning in Australia varies depending on the humidity, airflow, and temperature in the room. Typically, it takes between 4-6 hours for carpets to dry completely after steam cleaning.",
+    },
+    {
+        question: "How can I schedule a carpet cleaning service in Australia?",
+        answer: "It's easy to schedule a carpet cleaning service in Australia with Siyaan Cleaning. Simply contact us today to discuss your requirements and schedule your service at a time that is convenient for you."
+    },
+];
+
+const faqQuestion = carpetFAQ.map(faq => faq.question);
+console.log(faqQuestion);
